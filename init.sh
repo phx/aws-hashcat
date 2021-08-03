6da4e6e9796331cd3eec336a059a0ee04ad0fdc6
phx <oldjamey@gmail.com>
2021-08-03 13:44:22 -0500

    changed from bootstrap

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 sudo apt update -y && sudo apt install -y git &&\
 mkdir "$HOME/git" && cd "$HOME/git" &&\
 git clone https://github.com/phx/aws-hashcat &&\
-echo -e '\nsource "$HOME/git/aws-hashcat/scripts/01_install.sh"\n' >> ~/.bashrc
+source "$HOME/git/aws-hashcat/scripts/01_install.sh"
+
