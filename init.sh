0d2fe5d8d329821c7e6d4cf2c6d808a9f7d354f3
phx <oldjamey@gmail.com>
2021-08-03 13:45:08 -0500

    removed install git

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt install -y git &&\
 mkdir "$HOME/git" && cd "$HOME/git" &&\
 git clone https://github.com/phx/aws-hashcat &&\
 source "$HOME/git/aws-hashcat/scripts/01_install.sh"
